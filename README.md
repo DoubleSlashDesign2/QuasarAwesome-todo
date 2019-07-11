b732b973052efaa322daaf5779521ada6eb9268b
Larry Meadows <DoubleSlashDesign@gmail.com>
2019-07-10 22:38:32 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# QuasarAwesome-todo
-Quasar Awesome-todo app
+# Quasar App
+Quasar dev  to executein browser
+> WIP
+
+v-if removes a item from the DOM and v-show changes the display
+some methods are fire deven though they have nothing todo with what is going on.
+so you many need to use a computer property instead
+
+Computer methoods are treated as static property data and do not need the () like a method call
+
+Compted properties is more effecient than a method and they have FULL access to the DATA or other methoods
+filters have no access to the DATA or other methoods, they can only accepta value for processing
+and return some value, like formating dates or data in a text box
---
+++ src/pages/Index.vue
@@ -0,0 +1,66 @@
+<template padding>
+  <q-page padding>
+    <h5 class="border-grey" v-if="message.length">{{message}}</h5>
+    <h6 v-else>no message to show</h6>
+    <!-- <hr> -->
+    <p>
+      <input v-model="message" @keyup="handleKeyUp" />&nbsp;
+      <button @click="clearMessage">Clear</button>
+    </p>
+
+    <hr />
+    <p>Upper Case Computed: {{messageUpperCase}}</p>
+    <p>Upper Case Method: {{messageUpperCase1()}}</p>
+    <p>Lower Case Filter: {{message | messageLowerCase}}</p>
+
+    <!-- <input
+      v-model="message"
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage"
+      @mouseenter="alertMessage"
+    />-->
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "I Love Vue.Js"
+    };
+  },
+  computed: {
+    messageUpperCase() {
+      return this.message.toUpperCase();
+    }
+  },
+  methods: {
+    clearMessage() {
+      this.message = "";
+    },
+    handleKeyUp(event) {
+      if (event.keyCode == 27) this.clearMessage();
+      else console.log(event);
+    },
+    alertMessage() {
+      alert(this.message);
+    },
+    messageUpperCase1() {
+      return this.message.toUpperCase();
+    }
+  },
+  filters: {
+    messageLowerCase(value) {
+      return value.toLowerCase();
+    }
+  }
+};
+</script>
+
+<style>
+.border-grey {
+  border: 1px solid grey;
+  width: auto;
+  padding: 10px;
+}
+</style>
